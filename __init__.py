e83157343b6a7ff55f0c9f50e3d6ce5a21040fc1
TaikorRoy <furaoing@qq.com>
2015-09-10 14:04:12 +0800

    v1.2
    v1.2

--- __init__.py
+++ __init__.py
@@ -8,4 +8,5 @@ Created on Fri Jul 24 10:23:15 2015
 from system import get_content
 from system import listdir_enchanced
 from system import RunningTimer
+from system import running_timer
 
--- system.py
+++ system.py
@@ -6,7 +6,7 @@ Created on Fri Jul 24 10:23:52 2015
 """
 import os
 import time
-
+import codecs
 
 def listdir_enchanced(folder):
     files = os.listdir(folder)
@@ -17,22 +17,36 @@ def listdir_enchanced(folder):
     myfiles = sorted(myfiles)
     return myfiles
 
-
 def get_content(file, mode="read"):
     if mode == "read":
-        with open(file, "r", encoding="utf8") as f:
+        with codecs.open(file, "r", encoding="utf8") as f:
             s = f.read()
     if mode == "readlines":
-        with open(file, "r", encoding="utf8") as f:
-            s = f.read()
+        with codecs.open(file, "r", encoding="utf8") as f:
+            s = f.readlines()
     return s
 
+def get_content_list(file):
+    with codecs.open(file, "r", encoding="utf8") as f:
+        s = f.readlines()
+        for i in range(len(s)):
+            s[i] = s[i].strip("\n")
+    return s
     
 def write_content(file, content):
-    with open(file, "w", encoding="utf8") as f:
+    with codecs.open(file, "w", encoding="utf8") as f:
         f.write(content)
     return 0
 
+class running_timer:
+    def __init__(self):
+        self.start_time = time.time()
+        self.end_time = 0
+
+    def end(self):
+        self.end_time = time.time()
+        running_time = self.end_time - self.start_time
+        return running_time
 
 class RunningTimer:
     def __init__(self):
